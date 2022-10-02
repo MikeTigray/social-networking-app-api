73b0d586f677b3a5b850f18d0dd8cc5f8c9fd976
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-02 19:45:18 -0400

    created logic for getAllthoughts and createThought controllers

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -1,8 +1,22 @@
-const {} = require("../models");
+const { Thought } = require("../models");
 
 module.exports = {
-  getAllThoughts(req, res) {},
-  createThought(req, res) {},
+  getAllThoughts(req, res) {
+    Thought.find({}, (err, documents) => {
+      err ? res.status(404).json(err) : res.status(200).json(documents);
+    });
+  },
+  createThought(req, res) {
+    Thought.create(req.body)
+      .then((result) => {
+        res
+          .status(201)
+          .json({ status: "Thought created successfully 💭", thought: result });
+      })
+      .catch((err) => {
+        res.status(400).json({ status: "Though was NOT created!" });
+      });
+  },
   getSingleThought(req, res) {},
   updateThought(req, res) {},
   removeThought(req, res) {},
