005df0b5125e05065cfed4602ca1a4bbbf556fcb
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-02 03:31:15 -0400

    named user controller functions

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,1 +1,10 @@
 const {} = require("../models");
+module.exports = {
+  getAllUsers(req, res) {},
+  createUser(req, res) {},
+  getSingleUser(req, res) {},
+  updateUser(req, res) {},
+  removeUser(req, res) {},
+  addFriend(req, res) {},
+  removeFriend(req, res) {},
+};
--- routes/api/usersRoute.js
+++ routes/api/usersRoute.js
@@ -1,4 +1,16 @@
 const router = require("express").Router();
-const {} = require("../../controllers/usersController");
+const {
+  getAllUsers,
+  createUser,
+  getSingleUser,
+  updateUser,
+  removeUser,
+  addFriend,
+  removeFriend,
+} = require("../../controllers/usersController");
+
+router.route("/").get(getAllUsers).post(createUser);
+router.route("/:userId").get(getSingleUser).put(updateUser).delete(removeUser);
+router.route("/:userId/friends/:friendId").post(addFriend).delete(removeFriend);
 
 module.exports = router;
