f401eb8f32202c5ecfccf94ec8f97c75274ee5ae
Micheale Abay <michealeabay@Micheales-MacBook-Air.local>
2022-10-03 22:09:06 -0400

    delete thought from associated user

--- controllers/thoughtsController.js
+++ controllers/thoughtsController.js
@@ -41,16 +41,41 @@ module.exports = {
       )
       .catch((err) => res.status(400).json(err));
   },
-  removeThought(req, res) {
-    Thought.findOneAndDelete({ _id: req.params.thoughtId }).then((data) => {
-      if (!data) {
+  async removeThought(req, res) {
+    try {
+      // Finds the thought to be deleted
+      const thought = await Thought.findOne({ _id: req.params.thoughtId });
+      if (!thought) {
         res.status(200).json({ status: "No User with this Id" });
       }
+
+      // Finds the user associated to the thought
+      const associatedUser = User.findOne({ username: thought.username });
+      // Deletes the thought from user
+      const removeFromUser = await associatedUser.updateOne({
+        $pull: { thoughts: thought._id },
+      });
+      // Finally delete the thought document itself
+      const deleted = await thought.deleteOne();
+
       res.status(200).json({
         status: "Thought was deleted successfully!",
-        deletedThough: data,
+        deletedThough: deleted,
+        updatedUser: removeFromUser,
       });
-    });
+    } catch (error) {
+      res.status(400).json(error);
+    }
+
+    // Thought.findOneAndDelete({ _id: req.params.thoughtId }).then((data) => {
+    //   if (!data) {
+    //     res.status(200).json({ status: "No User with this Id" });
+    //   }
+    //   res.status(200).json({
+    //     status: "Thought was deleted successfully!",
+    //     deletedThough: data,
+    //   });
+    // });
   },
   async createReaction(req, res) {
     try {
--- models/index.js
+++ models/index.js
@@ -3,20 +3,20 @@ const { collection } = require("./user");
 const User = require("./user");
 const { userData, thoughtData, reactionData } = require("../utils/data");
 
-User.find({}).exec((err, collection) => {
-  if (collection.length === 0) {
-    User.insertMany(userData, (err, results) => {
-      err ? console.log(err) : console.log(results);
-    });
-  }
-});
+// User.find({}).exec((err, collection) => {
+//   if (collection.length === 0) {
+//     User.insertMany(userData, (err, results) => {
+//       err ? console.log(err) : console.log(results);
+//     });
+//   }
+// });
 
-Thought.find({}).exec((err, collection) => {
-  if (collection.length === 0) {
-    Thought.insertMany(thoughtData, (err, results) => {
-      err ? console.log(err) : console.log(results);
-    });
-  }
-});
+// Thought.find({}).exec((err, collection) => {
+//   if (collection.length === 0) {
+//     Thought.insertMany(thoughtData, (err, results) => {
+//       err ? console.log(err) : console.log(results);
+//     });
+//   }
+// });
 
 module.exports = { Thought, User, reactionSchema };
